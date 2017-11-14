0bebc087d364de8ae5c5334dcf8122d7900658fa
Philipp Tusch <philipp.tusch@huf-group.com>
2017-11-14 09:07:08 +0100

    1

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,15 @@
+'use strict';
+
+const danger = require('danger').danger;
+const warn = require('danger').warn;
+const _ = require('lodash');
+
+console.log(danger)
+
 // Add a CHANGELOG entry for app changes
-const hasChangelog = includes(danger.git.modified_files, "changelog.md")
-const isTrivial = contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
+const hasChangelog = _.includes(danger.git.modified_files, "changelog.md")
+const isTrivial = _.contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
 
 if (!hasChangelog && !isTrivial) {
   warn("Please add a changelog entry for your changes.")
 }
-
