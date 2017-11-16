87620a105371bfcd6419f199b7698ddc41c27074
Philipp Tusch <philipp.tusch@huf-group.com>
2017-11-16 10:48:23 +0100

    Even better dangerfile

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,32 @@
 'use strict';
 
+const fs = require('fs');
+
 const _ = require('underscore');
 
-// Add a CHANGELOG entry for app changes
+function untouchedChangelog() {
+  const hasChangelog = _.contains(danger.git.modified_files, "CHANGELOG.md")
+  const isTrivial = _.contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
 
-const hasChangelog = _.contains(danger.git.modified_files, "CHANGELOG.md")
-const isTrivial = _.contains((danger.github.pr.body + danger.github.pr.title), "#trivial")
 
-
-if (!hasChangelog && !isTrivial) {
-  warn("Please add a changelog entry for your changes.")
+  if (!hasChangelog && !isTrivial) {
+    warn('Your CHANGELOG.md seems to be untouched. Please make sure to add an entry if you touch code');
+  }
 }
+
+function incrementedVersionWithoutChangelog() {
+  const project = require('./package.json');
+  const version = new RegExp(project.version, 'g');
+
+  try {
+    const changelog = fs.readFile('CHANGELOG.md', 'utf8');
+    if (!version.test(changelog)) {
+      error('Looks like you tried to create a version without a changelog entry. You culprit!');
+    }
+  } catch (err) {
+
+  }
+}
+
+untouchedChangelog();
+incrementedVersionWithoutChangelog();
